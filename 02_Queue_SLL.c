c6e8725a3a68e8080b85fcb8c0ab0dcb19dc7191
Prateek Mittal <prateek.m-27@scds.saiuniversity.edu.in>
2025-11-21 22:21:04 +0530

    Add files via upload

--- 02_Queue_SLL.c
+++ 02_Queue_SLL.c
@@ -1,169 +1,105 @@
-/* Implementation of Queue using Singly Linked list  */
-#include <stdio.h>
-#include <stdlib.h>
-
-/* Global Variable Declarations */
+#include<stdio.h>
+#include<stdlib.h>
 struct node
 {
-  int key;
-  struct node *next;
+    int data;
+    struct node *next;
 };
+struct node *head=NULL;
+struct node *newnode=NULL;
+struct node *tail=NULL;
+ struct node  *temp=NULL;
+ struct node  *temp2=NULL;
+void create();
+void Display();
+void Append();
+void Delete();
+int main(void){
+    int option;
 
-struct node *L_head = NULL; // Empty List
-
-/* List operations */
-
-void printList();
-void ENQUEUE(int); 
-void DEQUEUE(); 
-void PEEK(); 
-struct node *createNode(int);
-
-int main(void)
-{
-  char option = '\0';
-
-  printf("------------------------\n");
-  printf("---Queue---\n");
-  printf("Show: S\n");
-  printf("ENQUEUE: 1\n");
-  printf("DEQUEUE: 2\n");
-  printf("PEEK: 3\n");
-  printf("EXIT: E\n");
-  printf("------------------------\n");
-
-  while (1)
-  {
-
-    printf("Enter your option: ");
-    scanf(" %c", &option);
-
-    switch (option)
-    {
-    case 'S':
-      printf("Displaying the queue.\n");
-      printList();
-
-      break;
-
-    case '1':
-      printf("Enter the element to enqueue: ");
-      int x = 0;
-      scanf("%d", &x);
-      ENQUEUE(x);
-
-      break;
-
-    case '2':
-      
-      DEQUEUE();
-
-      break;
-
-    case '3':
-      PEEK();
-      break;
-
+     printf("1------>Insert Element\n");
+     printf("2------>Delete\n");
+    printf("3------>Display\n");
+    printf("4------>To Exit\n");
+    while(1)
     
-    case 'E':
-      printf("Exit\n\n");
-      exit(0);
-
-    default:
-      printf("Enter correct option\n\n");
-    }
-  }
-
-  return 0;
-}
-
-struct node *createNode(int x)
-{
-  /* malloc the new node and typecast the address to struct node ptr. */
-  /* Set the key with x, prev and next to NULL */
-
-  struct node *newNode = (struct node *)malloc(1 * sizeof(struct node));
-  newNode->key = x;
-  newNode->next = NULL;
-
-  return newNode;
-}
-
-void printList()
-{
-  
-  if (L_head == NULL)
-  {
-    printf("Queue is empty\n");
-  }
-  else
-  {
-    printf("Queue elements:\n");
-    struct node *tmpHead = L_head;
-
-    while (tmpHead != NULL)
     {
-      printf("[%p] ==>  %d ----> %p\n",
-             tmpHead, tmpHead->key, tmpHead->next);
-      tmpHead = tmpHead->next;
+    
+    
+    printf("Enter your Option: ");
+            scanf("%d",&option);
+    
+        switch(option)
+        {
+            case 1:
+            Append();
+            break;
+            case 2:
+            Delete();
+            break;
+            case 3:
+            Display();
+            break;
+            case 4:
+            exit(0);
+            default:
+            printf("enter correct option\n");
+            break;
+        }
     }
-    printf("\n");
-  }
-
-  return;
+    
+    return 0;
 }
 
-void ENQUEUE(int x)
+
+void create()
 {
-    /* Navigate to the last node and update its next pointer to link the new node. */
-  /* If the list was empty, then head will point to the new node. */
+newnode=(struct node*)malloc(sizeof(struct node));
+newnode->next=NULL;
+newnode->data=0;
+}
 
-  struct node *newNode = createNode(x);
-
-  if (L_head == NULL)
-  {
-    L_head = newNode;
+void Display()
+{  
+    if(head==NULL)
+    {
+        printf("List is Empty\n");
+    }
+   else
+   {
+    temp=head; 
+    while(temp != NULL)
+    { 
+        printf("%d-->%p\n",temp->data,temp->next);
+        temp=temp->next;
+    }
+   }
+    
     return;
-  }
-
-  /* To navigate to the last node of current list */
-  struct node *lastNode = L_head;
-  while (lastNode->next != NULL)
-  {
-    lastNode = lastNode->next;
-  }
-
-  /* Next pointer of lastNode should point to the new node. */
-
-  lastNode->next = newNode;
-
-  return;
 }
 
-void DEQUEUE()
+
+void Append()
 {
-    if(L_head == NULL)
+    create();
+   printf("enter data to enter :");
+   scanf("%d",&newnode->data);
+   if(head== NULL)
     {
-        printf("Error! Underflow, Queue is empty. \n");
-    }
-    else if((L_head->next) == NULL)
-    {
-        printf("Element that had been dequeued is: %d \n",L_head->key);
-        L_head = NULL;
+        head=newnode;
+        tail=head;
     }
     else
-    {
-       printf("Element that had been dequeued is: %d \n",L_head->key);
-       L_head = L_head->next;
-    }
-    return;
+     {    
+     tail->next=newnode;
+     tail=newnode;
+     }
+     return;
 }
 
-void PEEK()
+void Delete(int x)
 {
-    printf("The element in the front is: %d \n",L_head->key);
-    return;
+  temp2=head;
+  head=temp2->next;
+  free(temp2);
 }
-
-
-
